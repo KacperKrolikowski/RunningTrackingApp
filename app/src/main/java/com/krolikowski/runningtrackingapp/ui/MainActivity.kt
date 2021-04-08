e071de186e1f2a0ba3670327b9a4837daadf0c83
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-04-08 10:13:14 +0200

    Navigation

--- app/src/main/java/com/krolikowski/runningtrackingapp/ui/MainActivity.kt
+++ app/src/main/java/com/krolikowski/runningtrackingapp/ui/MainActivity.kt
@@ -2,20 +2,30 @@ package com.krolikowski.runningtrackingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.krolikowski.runningtrackingapp.R
-import com.krolikowski.runningtrackingapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+                .addOnDestinationChangedListener { _, destination, _ ->
+                    when(destination.id){
+                        R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                            bottomNavigationView.visibility = View.VISIBLE
+                        else -> bottomNavigationView.visibility = View.INVISIBLE
+                    }
+                }
     }
 }
