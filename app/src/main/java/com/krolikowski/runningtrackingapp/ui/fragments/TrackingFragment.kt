3dd1c7f090f65b8c0397f098a243610218dca97a
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-04-08 13:41:46 +0200

    Start work under TrackingService.kt

--- app/src/main/java/com/krolikowski/runningtrackingapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/krolikowski/runningtrackingapp/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,14 @@
 package com.krolikowski.runningtrackingapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.krolikowski.runningtrackingapp.R
+import com.krolikowski.runningtrackingapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.krolikowski.runningtrackingapp.services.TrackingService
 import com.krolikowski.runningtrackingapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
@@ -22,11 +25,22 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.getMapAsync{
             map = it
         }
     }
 
+    private fun sendCommandToService(action: String) =
+            Intent(requireContext(), TrackingService::class.java).also {
+                it.action = action
+                requireContext().startService(it)
+            }
+
+    //Lifecycle of google maps
     override fun onResume() {
         super.onResume()
         mapView?.onResume()
